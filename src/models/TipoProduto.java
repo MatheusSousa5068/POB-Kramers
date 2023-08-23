ea8a78c45480a1210fa130438fe3519e810c5ac6
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-08-23 20:23:29 -0300

    adicionando models

---
+++ src/models/TipoProduto.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class TipoProduto {
+    private String nome;
+    private List<Produto> produtos = new ArrayList<>();
+
+    public TipoProduto(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionar(Produto p){
+        produtos.add(p);
+    }
+    public void remover(Produto p){
+        produtos.remove(p);
+    }
+
+    public Produto localizar(String nome){
+        for(Produto p: produtos)
+            if (p.getNome().equals(nome))
+                return p;
+        return null;
+    }
+}
---
+++ src/models/Venda.java
@@ -0,0 +1,64 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Venda {
+    private Integer id;
+    private String data;
+    private List<Produto> produtos = new ArrayList<>();
+    private double desconto;
+    private double valorpago;
+
+    public Venda(Integer id, String data, double desconto, double valorpago) {
+        this.id = id;
+        this.desconto = desconto;
+        this.valorpago = valorpago;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public double getValorpago() {
+        return valorpago;
+    }
+
+    public void setValorpago(double valorpago) {
+        this.valorpago = valorpago;
+    }
+
+    public void adicionar(Produto p){
+        produtos.add(p);
+    }
+    public void remover(Produto p){
+        produtos.remove(p);
+    }
+
+    public Produto localizar(String nome){
+        for(Produto p: produtos)
+            if (p.getNome().equals(nome))
+                return p;
+        return null;
+    }
+}
